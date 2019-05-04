46e31634153d3bbd03e20ca49ed24fe1669cdb17
B2Munchy <31100442+B2Munchy@users.noreply.github.com>
2019-05-04 18:22:14 +0200

    Added Comments & fixing a single bug

--- imid.py
+++ imid.py
@@ -1,4 +1,6 @@
+## Removed Guizero due to conflicts with Tkinter
 #from guizero import App, Text, PushButton
+#Importing required libraries
 import Tkinter
 from Tkinter import *
 import tkFileDialog as filedialog
@@ -9,12 +11,16 @@ import pytesseract
 from PIL import Image
 from pytesseract import image_to_string
 
-#cwd = os.getcwd() + "/imgs/"
+#Getting path to directory of the folder "imgs"
 cwd = os.path.dirname(os.getcwd()) + "/imgs/"
+
+#Fixing a tesseract path error by hardcoding its path 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 ## Path of working folder on Disk
 src_path = "C:\Users\B2M\Desktop\muntest\imageidentifier\imageidentifier\imgs/"
 
+#Main function that extracts text from image
 def get_string(img_path):
     # Read image with opencv
     img = cv2.imread(img_path)
@@ -30,6 +36,8 @@ def get_string(img_path):
     # Write image after removed noise
     cv2.imwrite(src_path + "/removed_noise.png", img)
 
+    # Disabled GAUSSIAN as it breaks certain images and conflicts with the functionality
+
     #  Apply threshold to get image with only black and white
     #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
@@ -44,43 +52,56 @@ def get_string(img_path):
     #os.remove(temp)
     os.remove(src_path + "/thres.png")
     os.remove(src_path + "/removed_noise.png")
-
     return result
+# Looping Over each image and combining the text in one string for the output
 def loop_imgs():
+    # Initializing 'final_text variable'
     final_text = ''
+    # Looping over each image in the 'imgs' folder
     for filename in os.listdir(cwd):
-        if filename.endswith(".png") or filename.endswith(".jpg"): 
+        # Currently only accepting png & jpg extensions
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            # Ignoring the temp files generated in case there is an error in deletion due to permission rights
             if filename != "thres.png" and filename != "removed_noise.png":
+                # Calling the main function and extracting the text
                 text = get_string(cwd + filename)
-                print('--- Start recognize text from image ---')
+                #print('--- Start recognize text from image ---') # For Debugging
                 #str = text.replace("S", "5")
+
+                # Combining Text extracted from each image
                 str = filename + " : \n" + text
                 final_text += str + '\n \n '
-                print("------ Done -------")
+                #print("------ Done -------")  # For Debugging
     return final_text
 
+# Callback function for the gui's button
 def button_action():
     res = loop_imgs()
+    # Setting the gui's text
     var.set(res)
-
+# Initializing trinkter's gui window
 window = Tk()
+# Initializing Label string value
 var = StringVar()
 
+# Setting the Title and Dimensions
 window.title('Image Identifier')
 window.geometry("700x600+10+10")
 
+# Button for starting the process and calling the function
 btn=Button(window, text="Analyze", fg='blue', command = button_action , font=("Helvetica", 12))
 btn.place(x=300, y=100)
 
+# Just a header title
 label = Label( window, text="Start the Identifing Process",  fg='red', font=("Helvetica", 16) )
 #label.place(x=500, y=500)
 
+# Output text
 lbl=Label(window, textvariable=var, fg='black', font=("Helvetica", 10))
 lbl.place(x=300, y=200)
 
-
-
-#label = Label( window, textvariable=var, relief=RAISED )
-#label.place(x=50, y=400)
+# Sending the label variable to interface
 label.pack()
+
+# Starting trinkter's interface mainloop
 window.mainloop()
--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 # setup.py
+
+# Used to build the application into a windows executable
+# Using py2exe
 from distutils.core import setup
 import py2exe
 setup(console=['imid.py'])
